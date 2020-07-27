2d051092cd6425e3a1cae5d9df754ed49657784b
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-27 20:00:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,15 +5,22 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  isa = {name: "Isa", age: 24, alignment: "left"}
+  isa = {
+    name: "Isa",
+    age: 24,
+    alignment: "left"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  pioneer = {name: "Grace Hopper"}
+  pioneer = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+
 end
